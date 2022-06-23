9599391451b55d4c4cffbdc5d87e2bb2103fbfd3
= <=>
2022-06-23 14:25:56 -0300

    adicionando ultimos arquivos do projeto

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express   = require('express')
+const exphbs    = require('express-handlebars')
 const app       = express();
+const path      = require('path')
 const db        = require('./db/connection')
 const bodyParser = require('body-parser');
+const Job        = require('./models/Job')
+const Sequelize = require('sequelize')
+const Op        = Sequelize.Op
 
 const PORT = 8000;
 
@@ -11,6 +16,18 @@ app.listen(PORT,function(){
 
 //body parser
 app.use(bodyParser.urlencoded({extended: false}));
+
+
+//handle-bars
+app.set('views',path.join(__dirname,'views'))
+
+app.engine('handlebars',exphbs.engine({defaultlayout: 'main'}))
+app.set('view engine','handlebars')
+
+//static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+
 
 //db connection
 db
@@ -22,9 +39,47 @@ db
     console.log('Ocorreu um erro ao conectar',err)
 })
   
+
 // routes
 app.get('/',(req,res)=>{
-    res.send('Está funcionando 2')
+
+
+    let search = req.query.job
+
+    let query = '%'+search+'%'
+
+    if(!search){
+        Job.findAll({order:[
+            ['createdAt','DESC']
+        ]})
+        .then(jobs =>{
+            res.render('index',{
+                jobs
+            })
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    
+    }else{
+        Job.findAll({
+            where: {title: {[Op.like]:query}},
+            order:[
+            ['createdAt','DESC']
+        ]})
+        .then(jobs =>{
+            res.render('index',{
+                jobs, search
+            })
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    }
+
+   
+
+  
 });
 
 // jobs routes
--- routes/jobs.js
+++ routes/jobs.js
@@ -3,9 +3,22 @@ const router  = express.Router()
 const Job = require('../models/Job');
 
 
+router.get('/add',(req,res)=>{
+    res.render('add')
+})
+
+//Detalhe da vaga
+router.get('/view/:id', (req,res)=> Job.findOne({
+    where: {id: req.params.id}
+}).then(job=>{
+    res.render('view',{
+        job
+    })
+})
+)
+
 
 // add job via post
-
 router.post('/add',(req,res)=>{
 
     let {title, salary, company, description, email, new_job} = req.body;
